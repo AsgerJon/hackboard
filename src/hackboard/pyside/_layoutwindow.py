15780cb8f9819d961cfc3f2815938420c19b6d60
AsgerJon <asgerjon2@gmail.com>
2023-05-28 11:51:21 +0200

    Base and layout window

--- src/hackboard/pyside/_layoutwindow.py
+++ src/hackboard/pyside/_layoutwindow.py
@@ -3,53 +3,44 @@
 #  MIT Licence
 from __future__ import annotations
 from PySide6.QtWidgets import QLabel, QLineEdit, QTextEdit, QVBoxLayout, \
-  QHBoxLayout, QWidget
+    QHBoxLayout, QWidget
 
 
 class LayoutWindow(BaseWindow):
-  """
-  A subclass of BaseWindow that provides layouts and widgets for a simple
-  word processing application.
+    """
+    A subclass of BaseWindow that provides layouts and widgets for a simple
+    word processing application.
 
-  This class adds a vertical layout to the QMainWindow and populates it
-  with a QLabel, a QLineEdit, and a QTextEdit.
-  The QLabel displays the current file name, the QLineEdit is used for
-  entering search terms, and the QTextEdit is used for editing text.
+    This class adds a vertical layout to the QMainWindow and populates it
+    with a QLabel, a QLineEdit, and a QTextEdit.
+    The QLabel displays the current file name, the QLineEdit is used for
+    entering search terms, and the QTextEdit is used for editing text.
+    """
 
-  Signals:
-  -------
-  None
+    def __init__(self):
+        super().__init__()
 
-  Slots:
-  -----
-  None
+        # Create widgets
+        self.file_label = QLabel("No file selected")
+        self.search_edit = QLineEdit()
+        self.text_edit = QTextEdit()
 
-  """
+        # Create layouts
+        main_layout = QVBoxLayout()
+        top_layout = QHBoxLayout()
+        bottom_layout = QHBoxLayout()
 
-  def __init__(self):
-    super().__init__()
+        # Add widgets to layouts
+        top_layout.addWidget(self.file_label)
+        top_layout.addWidget(self.search_edit)
+        bottom_layout.addWidget(self.text_edit)
 
-    # Create widgets
-    self.file_label = QLabel("No file selected")
-    self.search_edit = QLineEdit()
-    self.text_edit = QTextEdit()
+        main_layout.addLayout(top_layout)
+        main_layout.addLayout(bottom_layout)
 
-    # Create layouts
-    main_layout = QVBoxLayout()
-    top_layout = QHBoxLayout()
-    bottom_layout = QHBoxLayout()
+        # Create central widget and set layout
+        central_widget = QWidget()
+        central_widget.setLayout(main_layout)
 
-    # Add widgets to layouts
-    top_layout.addWidget(self.file_label)
-    top_layout.addWidget(self.search_edit)
-    bottom_layout.addWidget(self.text_edit)
-
-    main_layout.addLayout(top_layout)
-    main_layout.addLayout(bottom_layout)
-
-    # Create central widget and set layout
-    central_widget = QWidget()
-    central_widget.setLayout(main_layout)
-
-    # Set central widget
-    self.setCentralWidget(central_widget)
+        # Set central widget
+        self.setCentralWidget(central_widget)
